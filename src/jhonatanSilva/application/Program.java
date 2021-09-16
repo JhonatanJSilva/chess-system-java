bc77db393f87e0c812db82d17557c4a50f696dfe
Jhonatan Silva <jjsilvajlle@gmail.com>
2021-09-16 13:49:20 -0300

    Starting to implements Board and Piece

--- src/jhonatanSilva/application/Program.java
+++ src/jhonatanSilva/application/Program.java
@@ -1,14 +1,16 @@
 package jhonatanSilva.application;
 
+import jhonatanSilva.boardgame.Board;
 import jhonatanSilva.boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Board board = new Board(3, 5);
 
+        System.out.println(board.getColumns());
+        System.out.println(board.getRows());
     }
 
 }
